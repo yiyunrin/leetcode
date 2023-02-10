f5cb014529cc4a3199a79075f3420fb237eb4617
yiyun <58024602+yiyunrin@users.noreply.github.com>
2023-02-10 22:51:01 +0800

    Update As Far from Land as Possible.cpp

--- 1001~1500/1162/As Far from Land as Possible.cpp
+++ 1001~1500/1162/As Far from Land as Possible.cpp
@@ -3,31 +3,40 @@ public:
     int maxDistance(vector<vector<int>>& grid) 
     {
         int ans = 0, count = 0, n = grid.size();
-        queue<pair<int, int>> step;
-        for(int i = 0;i < n;++ i)
+        //count：計算現在grid中有幾個1
+        queue<pair<int, int>> step;    //存是1的座標
+        for(int i = 0;i < n;++ i)    //找原本grid裡的1
+        {
             for(int j = 0;j < n;++ j)
+            {
                 if(grid[i][j] == 1)
                 {
                     step.push({i, j});
                     ++ count;
                 }
+            }
+        }
 
-        while(!step.empty() && count != n * n)
+        while(!step.empty() && count != n * n)  //如果全部都是1就結束
         {
-            int tmp = step.size();
+            int tmp = step.size();    //算現在這個距離要做幾次
             ++ ans;
             for(int i = 0;i < tmp;i ++)
             {
                 auto [x, y] = step.front();
                 step.pop();
-                for(int j = max(0, x - 1);j <= min(x + 1, n - 1);++ j)
+                for(int j = max(0, x - 1);j <= min(x + 1, n - 1);++ j)    //上下左右
+                {
                     for(int k = max(0, y - 1);k <= min(y + 1, n - 1);++ k)
+                    {
                         if((j == x || k == y) && !grid[j][k])
                         {
                             step.push({j, k});
                             grid[j][k] = 1;
                             ++ count;
                         }
+                    }
+                }
             }
         } 
         return ans == 0 ? -1 : ans;

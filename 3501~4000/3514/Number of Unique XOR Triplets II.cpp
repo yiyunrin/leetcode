daf4b5b51bdb5466ebcafd71cab5642d0c00fb34
yiyun <58024602+yiyunrin@users.noreply.github.com>
2026-07-24 14:31:31 +0800

    add comment
    Optimize unique XOR triplets calculation by using a set to store intermediate results, reducing redundant computations.

--- 3501~4000/3514/Number of Unique XOR Triplets II.cpp
+++ 3501~4000/3514/Number of Unique XOR Triplets II.cpp
@@ -1,15 +1,19 @@
+// 暴力解會TLE
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
         unordered_set<int> pair, ans;
         for(int i = nums.size() - 1;i >= 0;i --){
+            // 紀錄後面的所有pair的解果
             for(int j = i;j < nums.size();j ++){
                 pair.insert(nums[i] ^ nums[j]);
             }
+            // 直接跟紀錄的pair結果做XOR運算，避免重複的pair運算
             for(int tmp: pair){
                 ans.insert(nums[i] ^ tmp);
             }
         }
+        // ans的大小即為答案
         return ans.size();
     }
 };
